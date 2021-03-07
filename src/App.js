ddfbf13f0a90d27a4a723ba53483e30d833e7567
Ihor Kharchenko <ihor.stavr@gmail.com>
2021-03-07 23:46:42 +0200

    hw 2_2 begining

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Phonebook from './components/Phonebook/Phonebook';
+import { v4 as uuidv4 } from 'uuid';
 
-function App() {
-  return (
-    <div className="App">
-      <img src={logo} className="App-logo" alt="logo" />
-    </div>
-  );
+class App extends Component {
+  state = {
+    contacts: [],
+  };
+  addContact = ({ name, number }) => {
+    // console.log(data);
+    const contact = {
+      name: name,
+      number: number,
+      id: uuidv4(),
+    };
+    this.setState(({ contacts }) => ({
+      contacts: [contact, ...contacts],
+    }));
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <img src={logo} className="App-logo" alt="logo" />
+        <Phonebook onSubmit={this.addContact} />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Phonebook/Phonebook.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+
+class Phonebook extends Component {
+  state = {
+    contacts: [],
+    name: '',
+    number: '',
+  };
+  handleChange = event => {
+    const { name, value } = event.currentTarget;
+    this.setState({ [name]: value });
+  };
+  handleSubmit = event => {
+    event.preventDefault();
+    this.props.onSubmit(this.state);
+    this.reset();
+  };
+  reset = () => {
+    this.setState({ name: '', number: '' });
+  };
+  render() {
+    return (
+      <>
+        <h1>Phonebook</h1>
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            Name
+            <input
+              type="text"
+              name="name"
+              value={this.state.name}
+              onChange={this.handleChange}
+            />
+          </label>
+          <label>
+            Number
+            <input
+              type="tel"
+              name="number"
+              value={this.state.number}
+              onChange={this.handleChange}
+            />
+          </label>
+          <button type="submit">Add contact</button>
+        </form>
+        <h2>Contacts</h2>
+      </>
+    );
+  }
+}
+export default Phonebook;
